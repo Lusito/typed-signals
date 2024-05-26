f5ec60d3d1bd31d944a3e0e710ea118e55dff977
Santo Pfingsten <core@lusito.info>
2024-05-26 17:38:36 +0200

    updated docs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { CollectorArray } from "./CollectorArray";
 export { CollectorLast } from "./CollectorLast";
 export { CollectorUntil0 } from "./CollectorUntil0";
 export { CollectorWhile0 } from "./CollectorWhile0";
-export { Signal } from "./Signal";
+export { Signal, ConnectOptions } from "./Signal";
 export { SignalConnection } from "./SignalConnection";
 export { SignalConnections } from "./SignalConnections";
